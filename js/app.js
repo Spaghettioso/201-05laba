76c0efd7fd16a7b05c605ddb3d20af32056c9a40
Jonathan OReilly <jonathanoreilly@live.co.uk>
2023-06-18 21:55:15 +0100

    q5 finished

--- js/app.js
+++ js/app.js
@@ -112,11 +112,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+let productFirstSecond = multiply(multArr[0], multArr[1])[0]
+let frstElement = multiply(productFirstSecond, multArr[2])[0]
+let secndElement = "The numbers " + multArr + " have a product of " + frstElement + "."
+let q5answer = [frstElement, secndElement]
+return q5answer
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
